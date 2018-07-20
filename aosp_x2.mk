960647ff0b93b53d085103b23167cabdd47423fb
shivatejapeddi <shivatejapeddi@gmail.com>
2018-07-20 17:42:24 +0000

    bring up Aex

--- aosp_x2.mk
+++ aosp_x2.mk
@@ -17,13 +17,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from device
+# Inherit from z2_plus device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxxhdpi
+TARGET_BOOT_ANIMATION_RES := 1440
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_x2
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_x2
 PRODUCT_DEVICE := x2
 PRODUCT_MANUFACTURER := LeEco
 PRODUCT_BRAND := LeEco
@@ -38,3 +42,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := LeEco/LeMax2_WW/le_x2:6.0.1/FKXOSOP5801910311S/letv10310125:user/release-keys
 
 TARGET_VENDOR := leeco
+
+EXTENDED_BUILD_TYPE=OFFICIAL
